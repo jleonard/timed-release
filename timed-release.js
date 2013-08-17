70a976b018b4c59ea5564e1635571ec6516b38b1
jleonard <thejohnleonard@gmail.com>
2013-08-17 08:05:43 -0400

    v1

--- timed-release.js
+++ timed-release.js
@@ -1,31 +1,24 @@
-(function(){ 
-  /* code */ 
+
   var TimedRelease = function(array,config,callback){
-  	this.config = $.extend({}, TimedRelease.DEFAULTS, config);
-  	this.currentIndex = 0;
-  	this.interval;
-  	this.t;
-  	this.loadItem();
+
+    var currentIndex = 0;
+    var interval;
+    var timer;
+
+    var loadItem = function(){
+      interval =  Math.floor(Math.random() * config.minInterval) + config.maxInterval;
+      timer = setTimeout(fireItem,interval);
+    }
+
+    var fireItem = function(){
+      callback(array[currentIndex],currentIndex);
+      if(currentIndex < array.length - 1){
+        currentIndex++;
+        loadItem();
+      }else{
+        if(config.loop){ currentIndex = 0; loadItem(); }
+      }
+    }
+    loadItem();
   }
 
-  TimedRelease.DEFAULTS = {
-    minInterval: 500, // the minimum interval between firing messages
-    maxInterval: 5000 // the maximum...
-  }
-
-  TimedRelease.prototype.loadItem = function(){
-  	this.interval = Math.floor(Math.random() * this.config.minInterval) + this.config.maxInterval;
-  	this.t = setTimeout(this.fireItem,this.interval);
-  }
-
-  TimedRelease.prototype.fireItem = function(){
-
-	callback(array[currentIndex],currentIndex);
-	if(currentIndex < array.length){
-		currentIndex++;
-		this.loadItem();
-	}
-
-  }
-
-})(window.jQuery);
