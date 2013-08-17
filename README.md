eccf827b164f0db4a5eb55470b79e527407f3884
jleonard <thejohnleonard@gmail.com>
2013-08-17 15:06:00 -0400

    adding jsDoc and readme documentation

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 #Timed Release
-A small .js utility used to simulate real time activity in an html simulation. You pass in an array of 
+A small .js utility to simulate real time activity in an html simulation. 
+
+#### Configuration
+The config argument has three optional properties.
+**minInterval** (milliseconds) The minimum amount of time between each release.
+**maxInterval** (milliseconds) The maximum amount of time between each release.
+**loop** (boolean) _optional_ if set to true the releases will continuously loop through the items in the array.
--- timed-release.js
+++ timed-release.js
@@ -1,15 +1,35 @@
+/**
+ * The TimedRelease constructor accepts a callback argument that fires on each release.
+ *
+ * @callback timedReleaseCallback
+ * @param {*} item - current array item being released
+ * @param {number} index - current array index
+ */
 
+/**
+ * Represents a TimedRelease object.
+ * @constructor
+ * @param {array} array - The array of items to release at random intervals
+ * @param {object} config - The configuration obj containing minInterval, maxInterval and optional loop properties.
+ * @param {timedReleaseCallback} callback - Called on each item release. 
+ */
   var TimedRelease = function(array,config,callback){
 
-    var currentIndex = 0;
-    var interval;
-    var timer;
+    var currentIndex = 0; // The current array index.
+    var interval; // The random number generated for each release.
+    var timer; // The setTimeout used to fire each release.
 
+    /**
+    * Preps a release. Sets the random interval time and resets the timer.
+    */
     var loadItem = function(){
       interval =  Math.floor(Math.random() * config.minInterval) + config.maxInterval;
       timer = setTimeout(fireItem,interval);
     }
 
+    /**
+    * Releases an item via the callback. Advances the current index or resets if the config is set to loop.
+    */
     var fireItem = function(){
       callback(array[currentIndex],currentIndex);
       if(currentIndex < array.length - 1){
